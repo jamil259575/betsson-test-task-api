8be9884902a2e10e59f88c0bb7f4dd0119eb91cb
Jamil Gurbanzade <jamil.gurbanzade@toptal.com>
2025-03-05 16:56:42 +0200

    apply prettier

--- tests/api/user/DELETE/user-delete.spec.ts
+++ tests/api/user/DELETE/user-delete.spec.ts
@@ -1,17 +1,23 @@
 import { test } from "../../fixtures/user-fixture";
 import { expect } from "@playwright/test";
-import {nonExistentUser, nonSpecifiedUser} from "../../../../paths/user/DELETE/user-delete";
+import {
+  nonExistentUser,
+  nonSpecifiedUser,
+} from "../../../../paths/user/DELETE/user-delete";
 
 test.describe.serial("DELETE /user/{username} API Tests", () => {
-
-  test("Should return error when deleting a non-existent user", async ({ apiFixture }) => {
+  test("Should return error when deleting a non-existent user", async ({
+    apiFixture,
+  }) => {
     await test.step("Send DELETE request for a user that does not exist", async () => {
       const response = await apiFixture.delete(nonExistentUser);
       expect(response.status()).toBe(404);
     });
   });
 
-  test("Should return error when deleting without specifying a username", async ({ apiFixture }) => {
+  test("Should return error when deleting without specifying a username", async ({
+    apiFixture,
+  }) => {
     await test.step("Send DELETE request without a username", async () => {
       const response = await apiFixture.delete(nonSpecifiedUser);
       expect(response.status()).not.toBe(200);
